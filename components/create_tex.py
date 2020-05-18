f5c5e67cdc6b6a242d3c3c424445dc0296c71292
Felipe Boff Nunes <felipeboffnunes@protonmail.com>
2020-05-18 12:47:31 -0300

    create all tex categories

--- components/create_tex.py
+++ components/create_tex.py
@@ -3,15 +3,20 @@ from textwrap import dedent
 MILLION = 7
 
 
-def create_tex(template):
+def create_all_tex(template):
+    all_tex = []
+    for category in template.CATEGORIES:
+        all_tex.append(create_tex(template, category))
+    return all_tex
 
+def create_tex(template, category):
+    # Needs dinamic logic
     layout = ""
-    layout += dedent(template.create_documentclass("title"))
+    layout += dedent(template.create_documentclass(category))
     layout += dedent(template.create_begin_document())
     layout += dedent(template.create_title())
     layout += dedent(template.create_maketitle())
     layout += dedent(template.create_end_document())
-
     return layout
 
 def download_tex(idx, tex):

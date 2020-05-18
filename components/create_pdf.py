c480d8605544713a50cf4f3d7a437a010ac01c58
Felipe Boff Nunes <felipeboffnunes@protonmail.com>
2020-05-18 15:05:45 -0300

    split pdfs, move originals and detect shapes(initial)

--- components/create_pdf.py
+++ components/create_pdf.py
@@ -2,9 +2,9 @@ import tempfile
 import subprocess
 import os
 
-WORK_DIR = "../../work_dir"
+TMP_PDF_PATH = "../../tmp_pdf"
 
 def create_pdf(tex_path):
     with tempfile.TemporaryDirectory() as tmp_dir:
         subprocess.run(["pdflatex", tex_path, "-interaction=nonstopmode",\
-            f"-aux-directory={tmp_dir}", f"-output-directory={WORK_DIR}"])
+            f"-aux-directory={tmp_dir}", f"-output-directory={TMP_PDF_PATH}"])
--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
+# Python Standard Libraries
 import argparse
 import tempfile
 import os
-
+# Libraries
+from pdf2image import convert_from_path
+# Components
 from components.select_layout import select_layout
 from components.create_tex import create_all_tex, download_tex
 from components.create_pdf import create_pdf
+from components.detect_shapes import detect_shapes
 
 def main(n, b):
 
@@ -14,8 +18,10 @@ def main(n, b):
 
     ORIGINAL_PATH = os.getcwd()
     TMP_TEX_PATH = f"{ORIGINAL_PATH}\\data\\tmp_tex"
+    TMP_PDF_PATH = f"{ORIGINAL_PATH}\\data\\tmp_pdf"
     TEMPLATE_PATH = f"{ORIGINAL_PATH}\\data\\template_src\\acm"
     TEX_PATH = f"{ORIGINAL_PATH}\\results\\tex"
+    PDF_PATH = f"{ORIGINAL_PATH}\\results\\pdf"
 
     # Batch iteration
     iterations = n//b
@@ -31,6 +37,11 @@ def main(n, b):
         # category with black boxes
         for idx, tex_categories in zip(batch_ids, batch_texs):
             # tex_categories = [tex, category]
+
+            # NEEDS TO CHANGE TMP DIRS TO REAL TMP DIRS!
+            # with tempfile.TemporaryDirectory() as path:
+
+            # Go to /data/tmp_tex and create texs
             os.chdir(TMP_TEX_PATH)
             tex_names = list(map( \
                 lambda tex_category : download_tex(idx, tex_category[0], tex_category[1]), tex_categories \
@@ -40,12 +51,33 @@ def main(n, b):
                 lambda tex_name : f"{TMP_TEX_PATH}\\{tex_name}", tex_names \
                 ))
         
+            # Go to /data/template_src/{template}/ and create PDFs
             os.chdir(TEMPLATE_PATH)
             list(map(create_pdf, tex_paths))
+
+            
             # Move the original tex to /results/tex folder
-            os.rename(tex_paths[0], f"{TEX_PATH}{tex_names[0]}".replace("_original", ""))
+            try:
+                os.rename(tex_paths[0], f"{TEX_PATH}\\{tex_names[0]}".replace("_original", ""))
+            except:
+                pass
             for tex_path in tex_paths[1:]:
                 os.remove(tex_path)
+
+            # Split PDFs into individual pages
+            with os.scandir(TMP_PDF_PATH) as pdfs:
+                for pdf in pdfs:
+                    if "_original" in pdf.name:
+                        # Move the original pdf to /results/pdf folder
+                        try:
+                            os.rename(pdf.path, f"{PDF_PATH}\\{pdf.name}".replace("_original", ""))
+                        except:
+                            pass
+                    else:
+                        with tempfile.TemporaryDirectory() as path:
+                            pages = convert_from_path(pdf.path, output_folder=path)
+                            list(map(detect_shapes, pages))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
